bbb22e0b2f40d5e3852d56d4364427c69609d48e
algirdas.urbonavicius <algirdas.urbonavicius@gmail.com>
2023-02-17 19:19:57 +0200

    Added responsive navigation

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const endpoint = "https://api.shrtco.de/v2/shorten";
+
+// function getrandom() {
+//   let random_string =
+//     Math.random().toString(32).substring(2, 5) +
+//     Math.random().toString(32).substring(2, 5);
+//   return random_string();
+// }
+
+
+///////////////
+
